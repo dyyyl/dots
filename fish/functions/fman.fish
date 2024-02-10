02dd026d0902b51d3a70ae8809a3a8bfe345ddf2
dyyyl <hey@dyyyl.rocks>
2024-02-10 08:46:43 -0500

    🐟🐟🐟

--- fish/functions/fman.fish
+++ fish/functions/fman.fish
@@ -1,4 +1,3 @@
-function fman --wraps='complete -C | fzf | xargs man' --description 'alias fman=complete -C | fzf | xargs man'
-  complete -C | fzf | xargs man $argv
-        
+function fman --wraps="complete -C | fzf | xargs sh -c 'man $0'" --description "alias fman=complete -C | fzf | xargs sh -c 'man $0'"
+    complete -C | fzf | xargs sh -c 'man $0' # xargs is insanely weird and the only way to pass the first argument to man is via a subshell. Dumb.
 end

8d7d65d4ecd143f95d4a45bfb90c443fdf1e4762
dyyyl <hey@dyyyl.rocks>
2024-02-18 12:26:27 -0500

    more housekeeping

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -3,14 +3,19 @@ return {
   -- https://github.com/folke/noice.nvim
   {
     "folke/noice.nvim",
-    opts = {
-      lsp = {
-        hover = {
-          -- Set not show a message if hover is not available
-          -- ex: shift+k on Typescript code
-          silent = true,
-        },
-      },
-    },
+    opts = function(_, opts)
+      -- Set not show a message if hover is not available
+      -- ex: shift+k on Typescript code
+      opts.lsp.hover = {
+        silent = true,
+      }
+
+      -- Set border for hovering lsp docs
+      opts.presets = {
+        lsp_doc_border = true,
+      }
+
+      return opts
+    end,
   },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,63 @@
 -- Description: Treesitter plugin configs
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    -- Treesitter autotags for fun and profit
-    {
-      "windwp/nvim-ts-autotag",
-      config = function()
-        require("nvim-ts-autotag").setup()
-      end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "agda",
+        "astro",
+        "awk",
+        "bash",
+        "c",
+        "clojure",
+        "css",
+        "csv",
+        "dockerfile",
+        "diff",
+        "fish",
+        "gitignore",
+        "go",
+        "graphql",
+        "html",
+        "http",
+        "hurl",
+        "javascript",
+        "jq",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "nix",
+        "python",
+        "query",
+        "scala",
+        "sql",
+        "styled",
+        "svelte",
+        "regex",
+        "rust",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
     },
-  },
-  opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
-    autotag = { enable = true },
-    autopairs = { enable = true },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
   },
 }

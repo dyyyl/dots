95e58f2c6231cc479757ad2d421d72d08b474090
dyyyl <hey@dyyyl.rocks>
2024-02-19 09:52:16 -0500

    within bat (interlaced). within delta (interlaced)

--- fish/functions/fv.fish
+++ fish/functions/fv.fish
@@ -1,3 +1,3 @@
 function fv --wraps="fzf | awk '{print $1}' | xargs nvim" --description="fzf | awk '{print $1}' | xargs nvim"
-    fzf | awk '{print $1}' | xargs nvim
+    fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | awk '{print $1}' | xargs nvim
 end

18ce0ca745e95c5302d80a9998c68162bf70bc6e
dyyyl <hey@dyyyl.rocks>
2024-02-22 12:30:45 -0500

    fix function

--- fish/functions/fv.fish
+++ fish/functions/fv.fish
@@ -1,3 +1,3 @@
-function fv --wraps="fzf | awk '{print $1}' | xargs nvim" --description="fzf | awk '{print $1}' | xargs nvim"
-    fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | awk '{print $1}' | xargs nvim
+function fv --wraps="fd . | zf | awk '{print $1}' | xargs nvim" --description="fd . | fzf | awk '{print $1}' | xargs nvim"
+    fd . | fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | awk '{print $1}' | xargs nvim
 end

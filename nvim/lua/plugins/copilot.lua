360104c77011e08114ec185b7683352a34828f23
dyyyl <hey@dyyyl.rocks>
2024-04-21 17:48:43 -0400

    Revert "remove redundant config"
    This reverts commit 0349dd4b132cb69b2d195580e12c57a3fbbc238c.

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+-- Load Copilot plugin
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = true },
+    panel = { enabled = false },
+  },
+}

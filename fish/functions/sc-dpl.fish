7981ff82403b08a220d2223feef48c30c642fa33
dyyyl <hey@dyyyl.rocks>
2024-04-28 14:55:29 -0400

    dumb, weird

--- fish/functions/sc-dpl.fish
+++ fish/functions/sc-dpl.fish
@@ -1,4 +1,4 @@
 function sc-dpl --wraps='yt-dlp -f bestaudio --extract-audio --audio-format flac --audio-quality 0 ' --description 'alias sc-dpl=yt-dlp -f bestaudio --extract-audio --audio-format flac --audio-quality 0 '
     # pass sc link as argument
-    yt-dlp -f bestaudio --extract-audio --audio-format flac --audio-quality 0 $argv
+    yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 $argv
 end

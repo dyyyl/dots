f7bcaae7400aca6795590a17b2405325a818cbef
dyyyl <hey@dyyyl.rocks>
2024-08-28 17:56:15 -0400

    clean colorscheme config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,7 @@
 -- Description: colorscheme plugin configs
 return {
-  -- add catppuccin
-  { "catppuccin/nvim", name = "catppuccin" },
-
-  -- Configure LazyVim to load chosen colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-frappe",
-    },
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "catppuccin-frappe",
   },
 }

5c1412516d4e804827dd7bf7616f123424320902
dyyyl <hey@dyyyl.rocks>
2024-08-28 16:06:19 -0400

    broken, revisit when (more) relevant

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,11 +25,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.sql" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     -- Config language plugins

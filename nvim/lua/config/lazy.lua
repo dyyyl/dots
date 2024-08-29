2ae8d2ef9074cccb41957592b83bd20a2d785de1
dyyyl <hey@dyyyl.rocks>
2024-08-29 10:08:41 -0400

    12 factor as fuck

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     -- Config language plugins
+    { import = "lazyvim.plugins.extras.util.dot" }, -- Dotfiles
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
